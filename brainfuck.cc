eb8d6e51794cd41d0f0b0ca128cbb86cab282d3b
Jonathan Roth <jonathanrothcs@yahoo.com>
2024-10-23 13:09:50 +0200

    Added Token Threading Interpretation and Direct Threading Interpretation as well as Direct Threading Compilation and some braifnuck programs for benchmarking.

--- brainfuck.cc
+++ brainfuck.cc
@@ -6,6 +6,10 @@
 #include <stdexcept>
 #include <vector>
 
+#if !defined(__GNUC__) && !defined(__clang__)
+#error "Computed gotos are only supported with GCC/Clang!"
+#endif
+
 const size_t BUFFER_SIZE = 4096;
 
 const char *input_file = nullptr;
@@ -76,90 +80,292 @@ struct Instruction {
     size_t jump_ref;
 };
 
-class Compiler {
+// Input simply to small to make a significant diference
+class DirectThreadingCompiler {
   public:
     std::vector<Instruction> compile(const std::vector<unsigned char> &ops) {
+        static void *dispatch_table[256];
+        for (int i = 0; i < 256; i++) {
+            dispatch_table[i] = &&parse_unknown;
+        }
+
+        dispatch_table['>'] = &&parse_mv_right;
+        dispatch_table['<'] = &&parse_mv_left;
+        dispatch_table['+'] = &&parse_inc_val;
+        dispatch_table['-'] = &&parse_dec_val;
+        dispatch_table['.'] = &&parse_output;
+        dispatch_table[','] = &&parse_input;
+        dispatch_table['['] = &&parse_jmp_fwd;
+        dispatch_table[']'] = &&parse_jmp_back;
+
         std::vector<Instruction> bytecode;
         std::stack<size_t> loop_stack;
+        size_t i = 0;
+#define NEXT_CHAR                                                                                                      \
+    ++i;                                                                                                               \
+    if (i < ops.size())                                                                                                \
+        goto *dispatch_table[ops[i]];                                                                                  \
+    return bytecode;
+        if (ops.empty())
+            return bytecode;
+        goto *dispatch_table[ops[i]];
 
-        for (unsigned char op : ops) {
-            switch (op) {
-            case '.':
-                bytecode.push_back({OpCode::OUTPUT, 0});
-                break;
-            case ',':
-                bytecode.push_back({OpCode::INPUT, 0});
-                break;
-            case '-':
-                bytecode.push_back({OpCode::DEC_VAL, 0});
-                break;
-            case '+':
-                bytecode.push_back({OpCode::INC_VAL, 0});
-                break;
-            case '<':
-                bytecode.push_back({OpCode::MV_LEFT, 0});
-                break;
-            case '>':
-                bytecode.push_back({OpCode::MV_RIGHT, 0});
-                break;
-            case '[':
-                loop_stack.push(bytecode.size());
-                bytecode.push_back({OpCode::JUMP_FWD, 0});
-                break;
-            case ']':
-                if (!loop_stack.empty()) { // Don't throw an error here
-                    bytecode[loop_stack.top()].jump_ref = bytecode.size();
-                    bytecode.push_back({OpCode::JUMP_BACK, loop_stack.top()});
-                    loop_stack.pop();
-                }
-                break;
-            }
+    parse_mv_right:
+        bytecode.push_back({OpCode::MV_RIGHT, 0});
+        NEXT_CHAR;
+    parse_mv_left:
+        bytecode.push_back({OpCode::MV_LEFT, 0});
+        NEXT_CHAR;
+    parse_inc_val:
+        bytecode.push_back({OpCode::INC_VAL, 0});
+        NEXT_CHAR;
+    parse_dec_val:
+        bytecode.push_back({OpCode::DEC_VAL, 0});
+        NEXT_CHAR;
+    parse_output:
+        bytecode.push_back({OpCode::OUTPUT, 0});
+        NEXT_CHAR;
+    parse_input:
+        bytecode.push_back({OpCode::INPUT, 0});
+        NEXT_CHAR;
+    parse_jmp_fwd:
+        loop_stack.push(bytecode.size());
+        bytecode.push_back({OpCode::JUMP_FWD, 0});
+        NEXT_CHAR;
+    parse_jmp_back:
+        if (!loop_stack.empty()) { // dont throw error here
+            bytecode[loop_stack.top()].jump_ref = bytecode.size();
+            bytecode.push_back({OpCode::JUMP_BACK, loop_stack.top()});
+            loop_stack.pop();
         }
-        return bytecode;
+        NEXT_CHAR;
+    parse_unknown:
+        NEXT_CHAR;
     }
 };
 
-class Interpreter {
+// class Compiler {
+//   public:
+//     std::vector<Instruction> compile(const std::vector<unsigned char> &ops) {
+//         std::vector<Instruction> bytecode;
+//         std::stack<size_t> loop_stack;
+//
+//         for (unsigned char op : ops) {
+//             switch (op) {
+//             case '.':
+//                 bytecode.push_back({OpCode::OUTPUT, 0});
+//                 break;
+//             case ',':
+//                 bytecode.push_back({OpCode::INPUT, 0});
+//                 break;
+//             case '-':
+//                 bytecode.push_back({OpCode::DEC_VAL, 0});
+//                 break;
+//             case '+':
+//                 bytecode.push_back({OpCode::INC_VAL, 0});
+//                 break;
+//             case '<':
+//                 bytecode.push_back({OpCode::MV_LEFT, 0});
+//                 break;
+//             case '>':
+//                 bytecode.push_back({OpCode::MV_RIGHT, 0});
+//                 break;
+//             case '[':
+//                 loop_stack.push(bytecode.size());
+//                 bytecode.push_back({OpCode::JUMP_FWD, 0});
+//                 break;
+//             case ']':
+//                 if (!loop_stack.empty()) { // Don't throw an error here
+//                     bytecode[loop_stack.top()].jump_ref = bytecode.size();
+//                     bytecode.push_back({OpCode::JUMP_BACK, loop_stack.top()});
+//                     loop_stack.pop();
+//                 }
+//                 break;
+//             }
+//         }
+//         return bytecode;
+//     }
+// };
+
+class DirectThreadingInterpreter {
   private:
     TwoEndedTape tape;
 
   public:
     void interprete(const std::vector<Instruction> &bytecode) {
-        for (size_t pc = 0; pc < bytecode.size(); ++pc) {
-            const Instruction &instr = bytecode[pc];
-            switch (instr.op) {
-            case OpCode::OUTPUT:
-                putchar(tape.get_curr());
-                break;
-            case OpCode::INPUT:
-                tape.set_curr(std::cin.get());
-                break;
-            case OpCode::DEC_VAL:
-                tape.decrement();
-                break;
-            case OpCode::INC_VAL:
-                tape.increment();
-                break;
-            case OpCode::MV_LEFT:
-                tape.moveLeft();
-                break;
-            case OpCode::MV_RIGHT:
-                tape.moveRight();
-                break;
-            case OpCode::JUMP_FWD:
-                if (tape.get_curr() == 0) {
-                    pc = instr.jump_ref;
-                };
-                break;
-            case OpCode::JUMP_BACK:
-                if (tape.get_curr() != 0) {
-                    pc = instr.jump_ref;
-                };
-                break;
-            }
+        static void *dispatch_table[] = {&&do_mv_right, &&do_mv_left, &&do_inc_val, &&do_dec_val,
+                                         &&do_output,   &&do_input,   &&do_jmp_fwd, &&do_jmp_back};
+        size_t pc = 0;
+#define DISPATCH goto *dispatch_table[static_cast<int>(bytecode[pc].op)]
+#define NEXT                                                                                                           \
+    ++pc;                                                                                                              \
+    if (pc < bytecode.size())                                                                                          \
+        DISPATCH;                                                                                                      \
+    return;
+
+        DISPATCH;
+
+    do_mv_right:
+        tape.moveRight();
+        NEXT;
+
+    do_mv_left:
+        tape.moveLeft();
+        NEXT;
+    do_inc_val:
+        tape.increment();
+        NEXT;
+    do_dec_val:
+        tape.decrement();
+        NEXT;
+    do_output:
+        putchar(tape.get_curr());
+        NEXT;
+    do_input:
+        tape.set_curr(std::cin.get());
+    do_jmp_fwd:
+        if (tape.get_curr() == 0) {
+            pc = bytecode[pc].jump_ref;
         }
+        NEXT;
+    do_jmp_back:
+        if (tape.get_curr() != 0) {
+            pc = bytecode[pc].jump_ref;
+        }
+        NEXT;
     }
 };
+
+// class TokenThreadingInterpreter {
+//   private:
+//     TwoEndedTape tape;
+//     // one indirection more than using GNU extension with gotos
+//     using Handler = void (*)(TwoEndedTape &tape, const std::vector<Instruction> &bytecode, size_t &pc);
+//
+//   public:
+//     static const Handler dispatch_table[];
+//
+//     void interprete(const std::vector<Instruction> &bytecode) {
+//         size_t pc = 0;
+//         dispatch_table[static_cast<int>(bytecode[pc].op)](tape, bytecode, pc);
+//     }
+// };
+//
+// static void handle_mv_right(TwoEndedTape &tape, const std::vector<Instruction> &bytecode, size_t &pc) {
+//     tape.moveRight();
+//     ++pc;
+//     if (pc < bytecode.size()) {
+//         TokenThreadingInterpreter::dispatch_table[static_cast<int>(bytecode[pc].op)](tape, bytecode, pc);
+//     }
+// }
+//
+// static void handle_mv_left(TwoEndedTape &tape, const std::vector<Instruction> &bytecode, size_t &pc) {
+//     tape.moveLeft();
+//     ++pc;
+//     if (pc < bytecode.size()) {
+//         TokenThreadingInterpreter::dispatch_table[static_cast<int>(bytecode[pc].op)](tape, bytecode, pc);
+//     }
+// }
+//
+// static void handle_inc_val(TwoEndedTape &tape, const std::vector<Instruction> &bytecode, size_t &pc) {
+//     tape.increment();
+//     ++pc;
+//     if (pc < bytecode.size()) {
+//         TokenThreadingInterpreter::dispatch_table[static_cast<int>(bytecode[pc].op)](tape, bytecode, pc);
+//     }
+// }
+//
+// static void handle_dec_val(TwoEndedTape &tape, const std::vector<Instruction> &bytecode, size_t &pc) {
+//     tape.decrement();
+//     ++pc;
+//     if (pc < bytecode.size()) {
+//         TokenThreadingInterpreter::dispatch_table[static_cast<int>(bytecode[pc].op)](tape, bytecode, pc);
+//     }
+// }
+//
+// static void handle_output(TwoEndedTape &tape, const std::vector<Instruction> &bytecode, size_t &pc) {
+//     putchar(tape.get_curr());
+//     ++pc;
+//     if (pc < bytecode.size()) {
+//         TokenThreadingInterpreter::dispatch_table[static_cast<int>(bytecode[pc].op)](tape, bytecode, pc);
+//     }
+// }
+//
+// static void handle_input(TwoEndedTape &tape, const std::vector<Instruction> &bytecode, size_t &pc) {
+//     tape.set_curr(std::cin.get());
+//     ++pc;
+//     if (pc < bytecode.size()) {
+//         TokenThreadingInterpreter::dispatch_table[static_cast<int>(bytecode[pc].op)](tape, bytecode, pc);
+//     }
+// }
+//
+// static void handle_jmp_fwd(TwoEndedTape &tape, const std::vector<Instruction> &bytecode, size_t &pc) {
+//     if (tape.get_curr() == 0) {
+//         pc = bytecode[pc].jump_ref;
+//     } else {
+//         ++pc;
+//     }
+//     if (pc < bytecode.size()) {
+//         TokenThreadingInterpreter::dispatch_table[static_cast<int>(bytecode[pc].op)](tape, bytecode, pc);
+//     }
+// }
+//
+// static void handle_jmp_back(TwoEndedTape &tape, const std::vector<Instruction> &bytecode, size_t &pc) {
+//     if (tape.get_curr() != 0) {
+//         pc = bytecode[pc].jump_ref;
+//     } else {
+//         ++pc;
+//     }
+//     if (pc < bytecode.size()) {
+//         TokenThreadingInterpreter::dispatch_table[static_cast<int>(bytecode[pc].op)](tape, bytecode, pc);
+//     }
+// }
+//
+// const TokenThreadingInterpreter::Handler TokenThreadingInterpreter::dispatch_table[] = {
+//     handle_mv_right, handle_mv_left, handle_inc_val, handle_dec_val,
+//     handle_output,   handle_input,   handle_jmp_fwd, handle_jmp_back};
+
+// class Interpreter {
+//   private:
+//     TwoEndedTape tape;
+//
+//   public:
+//     void interprete(const std::vector<Instruction> &bytecode) {
+//         for (size_t pc = 0; pc < bytecode.size(); ++pc) {
+//             const Instruction &instr = bytecode[pc];
+//             switch (instr.op) {
+//             case OpCode::OUTPUT:
+//                 putchar(tape.get_curr());
+//                 break;
+//             case OpCode::INPUT:
+//                 tape.set_curr(std::cin.get());
+//                 break;
+//             case OpCode::DEC_VAL:
+//                 tape.decrement();
+//                 break;
+//             case OpCode::INC_VAL:
+//                 tape.increment();
+//                 break;
+//             case OpCode::MV_LEFT:
+//                 tape.moveLeft();
+//                 break;
+//             case OpCode::MV_RIGHT:
+//                 tape.moveRight();
+//                 break;
+//             case OpCode::JUMP_FWD:
+//                 if (tape.get_curr() == 0) {
+//                     pc = instr.jump_ref;
+//                 };
+//                 break;
+//             case OpCode::JUMP_BACK:
+//                 if (tape.get_curr() != 0) {
+//                     pc = instr.jump_ref;
+//                 };
+//                 break;
+//             }
+//         }
+//     }
+// };
 
 std::vector<unsigned char> read_program(FILE *stream) {
     std::vector<unsigned char> program;
@@ -211,13 +417,13 @@ int main(int argc, char *argv[]) {
     if (input_stream != stdin) {
         fclose(input_stream);
     }
-    Compiler compiler;
+    DirectThreadingCompiler compiler;
     std::vector<Instruction> bytecode = compiler.compile(ops);
 
     if (should_print_bytecode) {
         print_bytecode(bytecode);
     } else {
-        Interpreter interpreter;
+        DirectThreadingInterpreter interpreter;
         interpreter.interprete(bytecode);
     }
 
